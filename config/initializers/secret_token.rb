a2f9dacef49aeab3d7f1e8d2255ac6d22ca693d8
Алексей Овчинников <alexiss@cybernetlab.ru>
2014-01-18 06:25:32 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapItSample::Application.config.secret_key_base = '26e45ca7f90ce0542bbcb032faeedb4b3fa1925944fd5f36a35dee6f7f799e83044c61e175ee3c1ce0679446c3922bb795a233298770fad3cb29297023ff2cfa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapItSample::Application.config.session_store :cookie_store, key: '_bootstrap_it_sample_session'
